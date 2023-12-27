934abb259b68f51738e6ec099983a4e598293eae
Akanshu Aich <aichakanshu2003@gmail.com>
2023-12-27 23:23:17 +0530

    User Profile Model

--- society_expenditure/api/admin.py
+++ society_expenditure/api/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from .models import Exp, Content
+from .models import Exp, Content, UserProfile
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 # Register your models here.
 class ContentAdmin(admin.ModelAdmin):
     list_display = ('user' , 'event','Date' ,'Current_Balance', 'Total_Amount' , 'Total_Spent')
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user' , 'fats' , 'cult' , 'paracosm' , 'photogeeks' , 'tech' , 'vedant' , 'megaheartz' , 'tars' , 'ecell' , 'sports')
 admin.site.register(Exp)
 admin.site.register(Content , ContentAdmin)
+admin.site.register(UserProfile, UserAdmin)
--- society_expenditure/api/models.py
+++ society_expenditure/api/models.py
@@ -16,3 +16,20 @@ class Content(models.Model):
     Current_Balance = models.DecimalField(max_digits = 20 , decimal_places = 2 , default = 0.00)
     Total_Amount = models.DecimalField(max_digits = 20 , decimal_places = 2 , default = 0.00)
     Total_Spent = models.DecimalField(max_digits = 20 , decimal_places = 2 , default = 0.00)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(Exp, on_delete=models.CASCADE)
+    fats = models.BooleanField(default=False)
+    cult = models.BooleanField(default=False)
+    paracosm = models.BooleanField(default=False)
+    photogeeks = models.BooleanField(default=False)
+    tech = models.BooleanField(default=False)
+    vedant = models.BooleanField(default=False)
+    megaheartz = models.BooleanField(default=False)
+    tars = models.BooleanField(default=False)
+    ecell = models.BooleanField(default=False)
+    sports = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.user.username
